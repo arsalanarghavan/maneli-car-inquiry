1e82087e0618720e9da925b3f37a74c2204beaf2
Arsalan Arghavan <arsalanarghavan2014@gmail.com>
2025-09-22 16:16:46 +0330

    adding pagination to car shortcode

--- includes/shortcodes/class-product-editor-shortcode.php
+++ includes/shortcodes/class-product-editor-shortcode.php
@@ -14,7 +14,7 @@ class Maneli_Product_Editor_Shortcode {
     }
 
     /**
-     * AJAX handler for filtering/searching products.
+     * AJAX handler for filtering/searching products with pagination.
      */
     public function handle_filter_products_ajax() {
         check_ajax_referer('maneli_product_filter_nonce');
@@ -24,29 +24,44 @@ class Maneli_Product_Editor_Shortcode {
         }
 
         $search_term = isset($_POST['search']) ? sanitize_text_field(wp_unslash($_POST['search'])) : '';
+        $paged = isset($_POST['page']) ? absint($_POST['page']) : 1;
+        $products_per_page = 20;
         
         $args = [
-            'limit' => -1,
+            'limit' => $products_per_page,
+            'page' => $paged,
             'orderby' => 'title',
             'order' => 'ASC',
+            'paginate' => true, // Important for getting total counts
         ];
 
         if (!empty($search_term)) {
             $args['s'] = $search_term;
         }
 
-        $products = wc_get_products($args);
+        $query_result = wc_get_products($args);
 
         ob_start();
-        if (!empty($products)) {
-            foreach ($products as $product) {
+        if (!empty($query_result->products)) {
+            foreach ($query_result->products as $product) {
                 self::render_product_row($product);
             }
         } else {
-            echo '<tr><td colspan="4">هیچ محصولی با این مشخصات یافت نشد.</td></tr>';
+            echo '<tr><td colspan="4" style="text-align:center;">هیچ محصولی با این مشخصات یافت نشد.</td></tr>';
         }
         $html = ob_get_clean();
-        wp_send_json_success(['html' => $html]);
+
+        $pagination_html = paginate_links([
+            'base' => '#',
+            'format' => '?paged=%#%',
+            'current' => $paged,
+            'total' => $query_result->max_num_pages,
+            'prev_text' => '« قبلی',
+            'next_text' => 'بعدی »',
+            'type' => 'plain'
+        ]);
+
+        wp_send_json_success(['html' => $html, 'pagination_html' => $pagination_html]);
     }
 
     /**
@@ -58,6 +73,18 @@ class Maneli_Product_Editor_Shortcode {
         }
 
         ob_start();
+        
+        $products_per_page = 20;
+        $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+        
+        $initial_products_query = wc_get_products([
+            'limit' => $products_per_page,
+            'page' => $paged,
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'paginate' => true,
+        ]);
+        
         ?>
         <div class="maneli-inquiry-wrapper">
             
@@ -84,31 +111,42 @@ class Maneli_Product_Editor_Shortcode {
                 </thead>
                 <tbody id="maneli-product-list-tbody">
                     <?php
-                    $products = wc_get_products(['limit' => -1, 'orderby' => 'title', 'order' => 'ASC']);
-                    if (!empty($products)) {
-                        foreach ($products as $product) {
+                    if (!empty($initial_products_query->products)) {
+                        foreach ($initial_products_query->products as $product) {
                             self::render_product_row($product);
                         }
                     } else {
-                        echo '<tr><td colspan="4">هیچ محصولی یافت نشد.</td></tr>';
+                        echo '<tr><td colspan="4" style="text-align:center;">هیچ محصولی یافت نشد.</td></tr>';
                     }
                     ?>
                 </tbody>
             </table>
             <div id="product-list-loader" style="display:none; text-align:center; padding: 40px;"><div class="spinner is-active" style="float:none;"></div></div>
+            
+            <div class="maneli-pagination-wrapper" style="margin-top: 20px; text-align: center;">
+                 <?php
+                    echo paginate_links([
+                        'base' => '#',
+                        'format' => '?paged=%#%',
+                        'current' => $paged,
+                        'total' => $initial_products_query->max_num_pages,
+                        'prev_text' => '« قبلی',
+                        'next_text' => 'بعدی »',
+                    ]);
+                 ?>
+            </div>
         </div>
 
         <script type="text/javascript">
         jQuery(document).ready(function($) {
             
-            // Function to format numbers with thousand separators
             function formatNumber(n) {
                 let numStr = String(n).replace(/[^0-9]/g, '');
                 return numStr.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             }
 
-            function initializePriceInputs(selector) {
-                $(selector).each(function() {
+            function initializePriceInputs(container) {
+                $(container).find('.manli-price-input').each(function() {
                     let initialValue = $(this).val();
                     if (initialValue) {
                         $(this).val(formatNumber(initialValue));
@@ -116,12 +154,11 @@ class Maneli_Product_Editor_Shortcode {
                 });
             }
             
-            initializePriceInputs('.manli-price-input');
+            initializePriceInputs(document);
 
-            // Using event delegation for dynamic elements
+            // --- Event Delegation for Dynamic Elements ---
             $('#maneli-product-list-tbody').on('keyup', '.manli-price-input', function() {
-                let formattedValue = formatNumber($(this).val());
-                $(this).val(formattedValue);
+                $(this).val(formatNumber($(this).val()));
             });
 
             $('#maneli-product-list-tbody').on('change', '.manli-data-input', function() {
@@ -134,12 +171,12 @@ class Maneli_Product_Editor_Shortcode {
                     fieldValue = fieldValue.replace(/,/g, '');
                 }
 
-                const spinner = inputField.closest('td').find('.spinner');
-                if(!spinner.length) {
-                     inputField.after('<span class="spinner"></span>');
+                if (!inputField.next('.spinner').length) {
+                     inputField.after('<span class="spinner" style="vertical-align: middle; margin-right: 5px;"></span>');
                 }
                 
-                inputField.next('.spinner').addClass('is-active');
+                const spinner = inputField.next('.spinner');
+                spinner.addClass('is-active');
                 inputField.css('border-color', '#007cba');
 
                 $.post('<?php echo admin_url("admin-ajax.php"); ?>', {
@@ -149,47 +186,87 @@ class Maneli_Product_Editor_Shortcode {
                     field_type: fieldType,
                     field_value: fieldValue
                 }, function(response) {
-                    inputField.next('.spinner').removeClass('is-active');
+                    spinner.removeClass('is-active');
                     if (response.success) {
                         inputField.css('border-color', '#46b450');
-                        setTimeout(() => inputField.css('border-color', ''), 1500);
                     } else {
                         inputField.css('border-color', '#dc3232');
-                        setTimeout(() => inputField.css('border-color', ''), 2000);
                         console.error('Error:', response.data);
                     }
+                     setTimeout(() => inputField.css('border-color', ''), 2000);
                 });
             });
 
-            // AJAX Search
+            // --- AJAX Search and Pagination ---
+            var xhr;
             var searchTimeout;
+            
+            function fetch_products(page = 1, search = '') {
+                if (xhr && xhr.readyState !== 4) {
+                    xhr.abort();
+                }
+
+                $('#product-list-loader').show();
+                $('#maneli-product-list-tbody').css('opacity', 0.5);
+                $('.maneli-pagination-wrapper').css('opacity', 0.5);
+
+                xhr = $.ajax({
+                    url: '<?php echo admin_url("admin-ajax.php"); ?>',
+                    type: 'POST',
+                    data: {
+                        action: 'maneli_filter_products_ajax',
+                        _ajax_nonce: '<?php echo wp_create_nonce("maneli_product_filter_nonce"); ?>',
+                        search: search,
+                        page: page
+                    },
+                    success: function(response) {
+                        if (response.success) {
+                            $('#maneli-product-list-tbody').html(response.data.html);
+                            $('.maneli-pagination-wrapper').html(response.data.pagination_html);
+                            initializePriceInputs('#maneli-product-list-tbody');
+                        }
+                    },
+                    complete: function() {
+                        $('#product-list-loader').hide();
+                        $('#maneli-product-list-tbody').css('opacity', 1);
+                        $('.maneli-pagination-wrapper').css('opacity', 1);
+                    }
+                });
+            }
+
             $('#product-search-input').on('keyup', function() {
                 clearTimeout(searchTimeout);
                 var searchTerm = $(this).val();
-                
                 searchTimeout = setTimeout(function() {
-                    $('#product-list-loader').show();
-                    $('#maneli-product-list-tbody').html('');
-
-                    $.ajax({
-                        url: '<?php echo admin_url("admin-ajax.php"); ?>',
-                        type: 'POST',
-                        data: {
-                            action: 'maneli_filter_products_ajax',
-                            _ajax_nonce: '<?php echo wp_create_nonce("maneli_product_filter_nonce"); ?>',
-                            search: searchTerm
-                        },
-                        success: function(response) {
-                            if (response.success) {
-                                $('#maneli-product-list-tbody').html(response.data.html);
-                                initializePriceInputs('#maneli-product-list-tbody .manli-price-input');
-                            }
-                        },
-                        complete: function() {
-                             $('#product-list-loader').hide();
-                        }
-                    });
+                    fetch_products(1, searchTerm);
                 }, 500);
+            });
+
+            // Pagination click handler
+            $('.maneli-pagination-wrapper').on('click', 'a.page-numbers', function(e) {
+                e.preventDefault();
+                var pageUrl = $(this).attr('href');
+                var pageNum = 1;
+                
+                // Extract page number from href (e.g., ?paged=2)
+                var matches = pageUrl.match(/paged=(\d+)/);
+                if (matches) {
+                    pageNum = matches[1];
+                } else if (!$(this).hasClass('prev') && !$(this).hasClass('next')) {
+                     // Handle case where link is just the number
+                    pageNum = $(this).text();
+                } else {
+                    // For prev/next, we need to determine the current page and adjust
+                    let currentPage = parseInt($('.maneli-pagination-wrapper .page-numbers.current').text());
+                    if ($(this).hasClass('prev')) {
+                        pageNum = Math.max(1, currentPage - 1);
+                    } else {
+                        pageNum = currentPage + 1;
+                    }
+                }
+
+                var searchTerm = $('#product-search-input').val();
+                fetch_products(pageNum, searchTerm);
             });
 
         });
@@ -209,7 +286,7 @@ class Maneli_Product_Editor_Shortcode {
         ?>
         <tr>
             <td data-title="نام خودرو">
-                <strong><a href="<?php echo get_edit_post_link($product_id); ?>" target="_blank"><?php echo esc_html($product->get_name()); ?></a></strong>
+                <strong><a href="<?php echo get_edit_post_link($product_id); ?>" target="_blank" rel="noopener"><?php echo esc_html($product->get_name()); ?></a></strong>
             </td>
             <td data-title="قیمت کامل (تومان)">
                 <input type="text"
